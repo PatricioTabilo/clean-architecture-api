621672d7aba1fc9debbaa53b1dcd0e0ebbc0f9a2
Patricio <pj.tabilo@gmail.com>
2023-01-10 19:50:52 -0300

    feat: implementa la ruta de note a express

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import noteRouter from './routes/note.router';
 
 // Instancia el servidor
 const server: express.Application = express();
@@ -6,6 +7,9 @@ const port = 3000;
 
 // Middleware para parsear request JSON
 server.use(express.json());
+
+// Aplica el router como middleware
+server.use('/notes', noteRouter());
 
 server.listen(port, () => {
   console.log(`Server listening on: ${port}`);

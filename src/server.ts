da58e218a4bca2bfaffd0083415ef88edcec3f69
Patricio <pj.tabilo@gmail.com>
2023-01-12 11:36:31 -0300

    feat: respuesta 404 a rutas no implementadas

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,11 @@ server.use(express.json());
 // Aplica el router como middleware
 server.use('/notes', noteRouter());
 
+//404 mantener siempre como último middleware
+server.use((req, res) =>
+  res.status(404).send({ url: `${req.originalUrl} not found` })
+);
+
 server.listen(port, () => {
   console.log(`Server listening on: ${port}`);
 });

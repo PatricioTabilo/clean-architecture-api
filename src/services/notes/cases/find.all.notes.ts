0208cc4881dfa6ffea859f58e109a78ef102be68
Patricio Tabilo <pj.tabilo@gmail.com>
2023-03-24 11:00:33 -0300

    Update find.all.notes.ts

--- src/services/notes/cases/find.all.notes.ts
+++ src/services/notes/cases/find.all.notes.ts
@@ -2,8 +2,8 @@ import { Note } from '../interfaces/note.entity';
 import { NoteRepositoryRef } from '../interfaces/note.repo.ref';
 
 /*
- * Se define el caso de uso, aca se debe imnplementar la logica de negocio
- * debido a la inyeccion de dependencias el caso de uso queda aislado del
+ * Se define el caso de uso, aca se debe imnplementar se implementan procesos
+ * especificos debido a la inyeccion de dependencias el caso de uso queda aislado del
  * modelo, facilmente se puede testear de manera unitaria usando mocks
  */
 export class FindAllNotes {
